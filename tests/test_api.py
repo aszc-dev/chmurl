c02a2d5e2f65eea5b713d2c25d29543f489956e2
aszc-dev <hi@aszc.dev>
2026-09-15 13:02:55 +0200

    tests: rework api tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -12,7 +12,7 @@ TEST_URLS = [
     "https://sqids.org/playground#alphabet=abdefghjkpqstuvwxyzADEFGHJKLMNPRSTUVWXYZ2345679&minLength=4&customBlocklist=&defaultBlocklist=true&tab=table&tabTZN=false&tabTCN=false&tabTMN=false&encodeInput=1%2C+2%2C+3&decodeInput=",
 ]
 
-FIRST_THREE_IDS = ["f2Tj", "6UVW", "ygGg"]
+FIRST_IDS = ["f2Tj", "6UVW", "ygGg"]
 
 
 @pytest.fixture(scope="session")
@@ -20,49 +20,51 @@ def client():
     return APIClient()
 
 
-@pytest.mark.django_db
-def test_get_redirects_correctly(client: APIClient):
-    for i, long_url in enumerate(TEST_URLS):
-        expected_sqid = FIRST_THREE_IDS[i]
+@pytest.fixture
+def prefilled_db(db):
+    for long_url in TEST_URLS:
         address = long_url
         Url(address=address).save()
 
+
+@pytest.mark.django_db
+def test_get_redirects_correctly(client: APIClient, prefilled_db):
+    for i, expected_sqid in enumerate(FIRST_IDS):
         response = client.get(f"/{expected_sqid}/")
 
         assert response.status_code == 302
-        assert response["Location"] == address
+        assert response["Location"] == TEST_URLS[i]
 
 
 @pytest.mark.django_db
-@pytest.mark.parametrize("address", TEST_URLS)
-def test_post_creates(address, client: APIClient):
-    data = {"address": address}
+def test_post_creates(client: APIClient):
+    for address in TEST_URLS:
+        data = {"address": address}
 
-    response = client.post("/", data)
+        response = client.post("/", data)
 
-    assert response.status_code == 201
-    assert response.json()["address"] == address
+        assert response.status_code == 201
+        assert response.json()["address"] == address
 
 
 @pytest.mark.django_db
-def test_post_multiple_times(client: APIClient):
-    address = "https://szkolawchmurze.org/"
-    data = {"address": address}
+def test_post_multiple_times(client: APIClient, prefilled_db):
+    for address in TEST_URLS:
+        data = {"address": address}
 
-    _ = client.post("/", data)
-    _ = client.post("/", data)
-    response = client.post("/", data)
+        response = client.post("/", data)
 
-    assert response.status_code == 200
-    assert response.json()["address"] == address
-    assert len(Url.objects.all()) == 1
+        assert response.status_code == 200
+        assert response.json()["address"] == address
+        assert len(Url.objects.all()) == len(TEST_URLS)
 
 
 @pytest.mark.django_db
 def test_post_fails_non_url(client: APIClient):
-    address = "not-an-actual-url"
-    data = {"address": address}
+    for address in TEST_URLS:
+        malformed = address.replace(".", "-").replace("/", "-")
+        data = {"address": malformed}
 
-    response = client.post("/", data)
+        response = client.post("/", data)
 
-    assert response.status_code == 400
+        assert response.status_code == 400
