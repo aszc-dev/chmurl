9b2d9dac6b0157be4a11fb578afc67f3943b9eac
aszc-dev <hi@aszc.dev>
2026-09-15 12:39:47 +0200

    tests: unparameterize get test

--- tests/test_api.py
+++ tests/test_api.py
@@ -7,13 +7,12 @@ from urlapi.models import Url
 # def init_db(db):
 
 TEST_URLS = [
-    address
-    for address in [
-        "https://chmurkopedia.szkolawchmurze.org/pl/articles/11042977-podstawowe-informacje-o-platformie",
-        "https://www.django-rest-framework.org/api-guide/requests/#standard-httprequest-attributes",
-        "https://sqids.org/playground#alphabet=abdefghjkpqstuvwxyzADEFGHJKLMNPRSTUVWXYZ2345679&minLength=4&customBlocklist=&defaultBlocklist=true&tab=table&tabTZN=false&tabTCN=false&tabTMN=false&encodeInput=1%2C+2%2C+3&decodeInput=",
-    ]
+    "https://chmurkopedia.szkolawchmurze.org/pl/articles/11042977-podstawowe-informacje-o-platformie",
+    "https://www.django-rest-framework.org/api-guide/requests/#standard-httprequest-attributes",
+    "https://sqids.org/playground#alphabet=abdefghjkpqstuvwxyzADEFGHJKLMNPRSTUVWXYZ2345679&minLength=4&customBlocklist=&defaultBlocklist=true&tab=table&tabTZN=false&tabTCN=false&tabTMN=false&encodeInput=1%2C+2%2C+3&decodeInput=",
 ]
+
+FIRST_THREE_IDS = ["f2Tj", "6UVW", "ygGg"]
 
 
 @pytest.fixture(scope="session")
@@ -22,15 +21,16 @@ def client():
 
 
 @pytest.mark.django_db
-@pytest.mark.parametrize("address", TEST_URLS)
-def test_get_redirects_correctly(address, client: APIClient):
-    expected_sqid = "f2Tj"
-    Url(address=address).save()
+def test_get_redirects_correctly(client: APIClient):
+    for i, long_url in enumerate(TEST_URLS):
+        expected_sqid = FIRST_THREE_IDS[i]
+        address = long_url
+        Url(address=address).save()
 
-    response = client.get(f"/{expected_sqid}/")
+        response = client.get(f"/{expected_sqid}/")
 
-    assert response.status_code == 302
-    assert response["Location"] == address
+        assert response.status_code == 302
+        assert response["Location"] == address
 
 
 @pytest.mark.django_db
